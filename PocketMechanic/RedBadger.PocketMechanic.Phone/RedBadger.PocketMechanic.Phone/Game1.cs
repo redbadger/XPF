93278aa6c892b649162845f360d5853934daf39c
Stuart Harris <stuart.harris@red-badger.com>
2010-07-12 11:12:52 +0100

    Initial Solution setup.  WP7 Xna app, class library and associated specs projects

---
+++ PocketMechanic/RedBadger.PocketMechanic.Phone/RedBadger.PocketMechanic.Phone/Game1.cs
@@ -0,0 +1,42 @@
+namespace RedBadger.PocketMechanic.Phone
+{
+    using System;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Input;
+
+    public class Game1 : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+
+        public Game1()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+
+            // Frame rate is 30 fps by default for Windows Phone.
+            this.TargetElapsedTime = TimeSpan.FromTicks(333333);
+
+            // Pre-autoscale settings.
+            this.graphics.PreferredBackBufferWidth = 480;
+            this.graphics.PreferredBackBufferHeight = 800;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                this.Exit();
+            }
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ PocketMechanic/RedBadger.Xpf.Phone/Class1.cs
@@ -0,0 +1,13 @@
+namespace RedBadger.Xpf.Phone
+{
+    public class Class1
+    {
+        public bool IsTest
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
