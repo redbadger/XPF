5103a9e2cf384fdac38271f0b88b050d6a453f46
Stuart Harris <stuart.harris@red-badger.com>
2010-08-10 09:15:05 +0100

    In process of implementing 2-way binding - works in tests but does not compile in Phone project as DependencyPropertyDescriptor does not exist in Silverlight.  Need to use this approach http://agsmith.wordpress.com/2008/04/07/propertydescriptor-addvaluechanged-alternative/ but haven't got time right now to implement.  Will come back to this.

--- PocketMechanic/RedBadger.Xpf/Presentation/BindingService.cs
+++ PocketMechanic/RedBadger.Xpf/Presentation/BindingService.cs
@@ -4,12 +4,26 @@ namespace RedBadger.Xpf.Presentation
     using System.Windows;
     using System.Windows.Data;
 
+#if WINDOWS_PHONE
+    using Microsoft.Phone.Reactive;
+#endif
+
+    using RedBadger.Xpf.Internal;
+
     public class BindingService
     {
+        private readonly DependencyProperty dependencyProperty;
+
         private readonly FrameworkElement frameworkElement;
+
+        private readonly UIElement uiElement;
+
+        private IDisposable subscription;
 
         public BindingService(UIElement uiElement, DependencyProperty dependencyProperty)
         {
+            this.uiElement = uiElement;
+            this.dependencyProperty = dependencyProperty;
             if (uiElement == null)
             {
                 throw new ArgumentNullException("uiElement");
@@ -26,6 +40,13 @@ namespace RedBadger.Xpf.Presentation
         public void Is(Binding binding)
         {
             this.frameworkElement.SetBinding(BindingFrameworkElement.DependencyProperty, binding);
+            if (binding.Mode == BindingMode.TwoWay)
+            {
+                this.subscription =
+                    this.uiElement.FromDependencyProperty<object>(this.dependencyProperty).Subscribe(
+                        Observer.Create<object>(
+                            o => this.frameworkElement.SetValue(BindingFrameworkElement.DependencyProperty, o)));
+            }
         }
 
         public class BindingFrameworkElement : FrameworkElement
