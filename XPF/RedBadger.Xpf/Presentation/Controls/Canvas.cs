117551e05ff6652685ef078abc7cf5cbc1a7b529
David Wynne <d_wynne@yahoo.com>
2010-09-02 23:38:31 +0100

    Started Canvas.  Implemented MeasureOverride.

---
+++ XPF/RedBadger.Xpf/Presentation/Controls/Canvas.cs
@@ -0,0 +1,22 @@
+﻿namespace RedBadger.Xpf.Presentation.Controls
+{
+    using System.Windows;
+
+    public class Canvas : Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var infiniteAvailableSize = new Size(double.PositiveInfinity, double.PositiveInfinity);
+
+            foreach (var element in this.GetVisualChildren())
+            {
+                if (element != null)
+                {
+                    element.Measure(infiniteAvailableSize);
+                }
+            }
+
+            return new Size();
+        }
+    }
+}
