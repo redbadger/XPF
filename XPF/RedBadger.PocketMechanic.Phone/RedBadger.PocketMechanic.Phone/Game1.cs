995b97e306f810a05de6d8eab9d8e6f3e1a74cd6
David Wynne <d_wynne@yahoo.com>
2010-09-16 19:06:04 +0100

    Installed the release version of the WP7 dev tools and updated project references.

--- XPF/RedBadger.PocketMechanic.Phone/RedBadger.PocketMechanic.Phone/Game1.cs
+++ XPF/RedBadger.PocketMechanic.Phone/RedBadger.PocketMechanic.Phone/Game1.cs
@@ -21,7 +21,7 @@ namespace RedBadger.PocketMechanic.Phone
             this.graphics.PreferredBackBufferWidth = 480;
             this.graphics.PreferredBackBufferHeight = 800;
 
-            this.Components.Add(new GridTest(this));
+            this.Components.Add(new ScrollTest(this));
         }
 
         protected override void Draw(GameTime gameTime)
