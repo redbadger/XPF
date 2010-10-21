19e2ae776a237e7b3573336a9d345914ecdbf452
David Wynne <d_wynne@yahoo.com>
2010-10-21 12:35:39 +0100

    ToggleButton documentation.

--- XPF/RedBadger.Xpf/Controls/Primitives/ToggleButton.cs
+++ XPF/RedBadger.Xpf/Controls/Primitives/ToggleButton.cs
@@ -2,20 +2,44 @@ namespace RedBadger.Xpf.Controls.Primitives
 {
     using System;
 
+    /// <summary>
+    ///     Base class for all controls with toggling functionality.
+    /// </summary>
     public class ToggleButton : ButtonBase
     {
+        /// <summary>
+        ///     <see cref = "IsChecked">IsChecked</see> Reactive Property.
+        /// </summary>
         public static readonly ReactiveProperty<bool?> IsCheckedProperty = ReactiveProperty<bool?>.Register(
             "IsChecked", typeof(ToggleButton), false, OnIsCheckedPropertyChanged);
 
+        /// <summary>
+        ///     <see cref = "IsThreeState">IsThreeState</see> Reactive Property.
+        /// </summary>
         public static readonly ReactiveProperty<bool> IsThreeStateProperty =
             ReactiveProperty<bool>.Register("IsThreeState", typeof(ToggleButton));
 
+        /// <summary>
+        ///     Occurs when a <see cref = "ToggleButton">ToggleButton</see> is checked.
+        /// </summary>
         public event EventHandler<EventArgs> Checked;
 
+        /// <summary>
+        ///     Occurs when the state of a <see cref = "ToggleButton">ToggleButton</see> is neither checked nor unchecked.
+        /// </summary>
+        /// <remarks>
+        ///     This can only occur when <see cref = "IsThreeState">IsThreeState</see> is true.
+        /// </remarks>
         public event EventHandler<EventArgs> Indeterminate;
 
+        /// <summary>
+        ///     Occurs when a <see cref = "ToggleButton">ToggleButton</see> is unchecked.
+        /// </summary>
         public event EventHandler<EventArgs> Unchecked;
 
+        /// <summary>
+        ///     Gets or sets a value indicating whether a <see cref = "ToggleButton">ToggleButton</see> is in a checked, unchecked or indeterminate state.
+        /// </summary>
         public bool? IsChecked
         {
             get
@@ -29,6 +53,12 @@ namespace RedBadger.Xpf.Controls.Primitives
             }
         }
 
+        /// <summary>
+        ///     Gets or sets a value indicating whether a <see cref = "ToggleButton">ToggleButton</see> supports two or three states.
+        /// </summary>
+        /// <remarks>
+        ///     If false, <see cref = "IsChecked">IsChecked</see> can only be true or false.  If true, <see cref = "IsChecked">IsChecked</see> can enter a third null state.
+        /// </remarks>
         public bool IsThreeState
         {
             get
